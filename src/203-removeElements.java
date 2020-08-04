9de1cf0d1c6a2a7c4261a645644c9566b906255b
Aaronself <Aaronself74@outlook.com>
2020-08-04 21:49:35 +0800

    easy

---
+++ src/203-removeElements.java
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=java
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) { val = x; }
+}
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        while (pre.next != null) {
+            if (pre.next.val == val) {
+                pre.next = pre.next.next;
+            }
+            else
+                pre = pre.next;
+        }
+
+        return dummy.next;
+    }
+}
+// @lc code=end
+
